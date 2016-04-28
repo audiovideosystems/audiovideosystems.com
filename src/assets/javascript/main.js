df587aa32c64392cd25aef43fa4ffc85db931b99
Ryan Aponte <ryan@ryanaponte.com>
2016-04-28 17:05:37 -0400

    logo and ul styles

--- src/assets/javascript/main.js
+++ src/assets/javascript/main.js
@@ -51,37 +51,41 @@
   });
 
   // centered nav
-  // $(window).resize(function() {
-  //   var more = document.getElementById("js-centered-more");
-  //   var windowWidth = $(window).width();
-  //   var moreLeftSideToPageLeftSide = $(more).offset().left;
-  //   var moreLeftSideToPageRightSide = windowWidth - moreLeftSideToPageLeftSide;
+  $(window).on("load resize",function(e) {
+    var more = document.getElementById("js-centered-more");
 
-  //   if (moreLeftSideToPageRightSide < 330) {
-  //     $("#js-centered-more .submenu .submenu").removeClass("fly-out-right");
-  //     $("#js-centered-more .submenu .submenu").addClass("fly-out-left");
-  //   }
+    if ($(more).length > 0) {
+      var windowWidth = $(window).width();
+      var moreLeftSideToPageLeftSide = $(more).offset().left;
+      var moreLeftSideToPageRightSide = windowWidth - moreLeftSideToPageLeftSide;
 
-  //   if (moreLeftSideToPageRightSide > 330) {
-  //     $("#js-centered-more .submenu .submenu").removeClass("fly-out-left");
-  //     $("#js-centered-more .submenu .submenu").addClass("fly-out-right");
-  //   }
-  // });
+      if (moreLeftSideToPageRightSide < 330) {
+        $("#js-centered-more .submenu .submenu").removeClass("fly-out-right");
+        $("#js-centered-more .submenu .submenu").addClass("fly-out-left");
+      }
 
-  // $(document).ready(function() {
-  //   $(window).trigger("resize");
-  //   var menuToggle = $("#js-centered-navigation-mobile-menu").unbind();
-  //   $("#js-centered-navigation-menu").removeClass("show");
+      if (moreLeftSideToPageRightSide > 330) {
+        $("#js-centered-more .submenu .submenu").removeClass("fly-out-left");
+        $("#js-centered-more .submenu .submenu").addClass("fly-out-right");
+      }
+    }
 
-  //   menuToggle.on("click", function(e) {
-  //     e.preventDefault();
-  //     $("#js-centered-navigation-menu").slideToggle(function(){
-  //       if($("#js-centered-navigation-menu").is(":hidden")) {
-  //         $("#js-centered-navigation-menu").removeAttr("style");
-  //       }
-  //     });
-  //   });
-  // });
+    var menuToggle = $("#js-centered-navigation-mobile-menu").unbind();
+    $("#js-centered-navigation-menu").removeClass("show");
+
+    menuToggle.on("click", function(e) {
+      e.preventDefault();
+      $("#js-centered-navigation-menu").slideToggle(function(){
+        if($("#js-centered-navigation-menu").is(":hidden")) {
+          $("#js-centered-navigation-menu").removeAttr("style");
+        }
+      });
+      setTimeout(function(){
+        $('.centered-navigation').toggleClass('short');
+      }, 250);
+    });
+  });
+
 
 
 })();
